7edce8c5c012cbfb2e30bf46ed217b6460399f44
Jakob Werner <Jakob.werner5253@gmail.com>
2023-08-13 18:19:06 -0600

    piece model add, and init board

--- chess_app/models.py
+++ chess_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Chessboard(models.Model):
+    # Representation of 8x8 chessboard
+    squares = models.CharField(max_length=64, default=' ' * 64) # Initial empty board
+
+class Piece(models.Model):
+    PIECE_TYPE_CHOICES = [
+        ('K', 'King'),
+        ('Q', 'Queen'),
+        ('R', 'Rook'),
+        ('N', 'Knight'),
+        ('B', 'Bishop'),
+        ('P', 'Pawn'),
+    ]
+    PIECE_COLOR_CHOICES = [
+        ('W', 'White'),
+        ('B', 'Black'),
+    ]
+    type = models.CharField(max_length=1, choices=PIECE_TYPE_CHOICES)
+    color = models.CharField(max_length=1, choices=PIECE_COLOR_CHOICES)
+    position = models.PositiveIntegerField() # Position on the board (0-63)
+    chessboard = models.ForeignKey(Chessboard, on_delete=models.CASCADE)
--- chess_app/views.py
+++ chess_app/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
-# Create your views here.
+def initialize_board(chessboard):
+    # Define the initial positions of the pieces
+    initial_pieces = [
+        ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
+        ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+        ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+        ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
+    ]
+
+    # convert to a single string to store the whole board in one database field
+    chessboard.squares = ''.join([''.join(row) for row in initial_pieces])
+    chessboard.save()
+
+    # Create Piece instances for each piece on the board
+    for row_index, row in enumerate(initial_pieces):
+        for col_index, piece_char in enumerate(row):
+            if piece_char != ' ':
+                color = 'W' if piece_char.isupper() else 'B'
+                position = row_index * 8 + col_index
+                Piece.objects.create(
+                    type=piece_char.upper(),
+                    color=color,
+                    position=position,
+                    chessboard=chessboard
+                )
+
