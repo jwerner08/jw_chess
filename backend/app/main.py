939a5f87f6f1c0f7af39d8ae216936dc24907604
Jakob Werner <Jakob.werner5253@gmail.com>
2023-08-13 21:26:28 -0600

    move initialize board and update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from .models import Board  # Assuming models.py is in the same package
+from .chess_logic import initialize_board  # Assuming chess_logic.py is in the same package
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to the Chess App Backend!"}
+# Initialize the chessboard with the starting pieces
+current_board = initialize_board()
 
-# You can continue to define other endpoints related to chess gameplay here
+@app.get("/board", response_model=Board)
+def get_board():
+    return current_board
+
+@app.post("/reset")
+def reset_board():
+    global current_board
+    current_board = initialize_board()
+    return {"message": "Board reset"}
+
--- backend/app/models.py
+++ backend/app/models.py
@@ -8,23 +8,3 @@ class Piece(BaseModel):
 
 class Board(BaseModel):
     squares: List[List[Piece]]  # 8x8 grid representing the chessboard
-
-def initialize_board() -> Board:
-    initial_pieces = [
-        ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
-        ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
-        [None] * 8,
-        [None] * 8,
-        [None] * 8,
-        [None] * 8,
-        ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
-        ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
-    ]
-
-    squares = [
-        [Piece(type=piece, color='white') if piece and piece.isupper() else Piece(type=piece, color='black') if piece else None
-            for piece in row]
-        for row in initial_pieces
-    ]
-
-    return Board(squares=squares)
