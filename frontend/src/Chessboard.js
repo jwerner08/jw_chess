bbf2a2dd71584001a46ca7bcffff0953192edd06
Jakob Werner <Jakob.werner5253@gmail.com>
2023-08-13 20:45:25 -0600

    setup tailwind.css, add board rendering

---
+++ frontend/src/Chessboard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function Chessboard() {
+  const rows = 8;
+  const cols = 8;
+
+  // Generate the grid
+  const squares = [];
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      const isEvenRow = i % 2 === 0;
+      const isEvenCol = j % 2 === 0;
+      const isDarkSquare = (isEvenRow && !isEvenCol) || (!isEvenRow && isEvenCol);
+      const squareClass = isDarkSquare ? 'bg-black' : 'bg-white';
+
+      squares.push(<div key={`${i}-${j}`} className={`w-10 h-10 ${squareClass}`} />);
+    }
+  }
+
+  return (
+    <div className="flex flex-wrap w-80 h-80 center">
+      {squares}
+    </div>
+  );
+}
+
+export default Chessboard;
+
